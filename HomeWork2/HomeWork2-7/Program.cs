9e8e229579bb12b040398454ca3f98c2317b7fb5
Valery Kryukov <valery310@live.com>
2020-08-01 19:26:23 +0300

    Ex 2-7

--- HomeWork2/HomeWork2-7/Program.cs
+++ HomeWork2/HomeWork2-7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,32 @@ namespace HomeWork2_7
 
         static void Main(string[] args)
         {
+            RecNumber(1, 10);
+            Console.WriteLine($"Сумма чисел от 1 до 10: {SumNumber(1, 10)}");
+            Console.ReadKey();
+        }
+
+        public static void RecNumber(int a, int b) 
+        {          
+            Console.WriteLine(a);
+            if (a < b)
+            {
+                a++;
+                RecNumber(a, b);
+            }          
+        }
+
+        public static int SumNumber(int a, int b)
+        {
+            if (a < b)
+            {
+                return a + SumNumber(++a, b);
+            }
+            else
+            {
+                return a;
+            }
+            
         }
     }
 }
