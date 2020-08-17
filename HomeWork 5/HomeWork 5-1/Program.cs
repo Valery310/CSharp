f35cf926af507a22307d9ea977d0d2d1246242eb
Valeriy Kryukov <valery310@live.com>
2020-08-17 20:52:09 +0300

    Added Homework 5 and 6

---
+++ HomeWork 5/HomeWork 5-1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HomeWork_5_1
+{
+    class Program
+    {
+
+        //Создать программу, которая будет проверять корректность ввода логина.Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+        //а) без использования регулярных выражений;
+        //б) **с использованием регулярных выражений.
+        //КрюковВН
+
+        static void Main(string[] args)
+        {
+            string Login ="";
+            Console.WriteLine("Введите логин: ");
+            Login = Console.ReadLine();
+
+            Console.WriteLine("Проверка без использования регулярного выражения.");
+            if (checkLogin(Login))
+            {
+                Console.WriteLine("Логин корректен.");
+            }
+            else
+            {
+                Console.WriteLine("Логин некорректен!");
+            }
+
+            Console.WriteLine("Проверка с использованием регулярного выражения.");
+            if (checkLoginRegular(Login))
+            {
+                Console.WriteLine("Логин корректен.");
+            }
+            else
+            {
+                Console.WriteLine("Логин некорректен!");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool checkLogin(string Login) 
+        {
+            if (Login.Length > 2 && Login.Length < 10 )
+            {
+                int t;
+                if (!int.TryParse(Login.Substring(0,1), out t))
+                {
+                    for (int i = 0; i < Login.Length; i++)
+                    {
+                        if (!((Login[i] >= 'A' && Login[i] <= 'Z') || (Login[i] >= 'a' && Login[i] <= 'z') || (Login[i] >= '0' && Login[i] <= '9')))
+                        {
+                            return false;
+                        }                        
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static bool checkLoginRegular(string Login)
+        {
+            RegexOptions regexOp = RegexOptions.IgnoreCase;
+            
+            Regex regex = new Regex(@"^[a-zA-Z]{1}\w[a-zA-Z0-9]{0,8}$", regexOp);
+            
+            return regex.IsMatch(Login);
+        }
+    }
+}
---
+++ HomeWork 6/HomeWork 6-1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6_1
+{
+
+    public delegate double Fun(double x, double a);
+
+    class Program
+    {
+        //Изменить программу вывода таблицы функции так, чтобы можно было передавать 
+        //    функции типа double (double, double). Продемонстрировать работу на функции 
+        //    с функцией a* x^2 и функцией a* sin(x).
+        //    Крюков ВН
+
+        public static void Table(Fun F, double x, double a, double b)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, a));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+
+        public static double MyFunc1(double x, double a)
+        {
+            return a * Math.Sqrt(x);
+        }
+
+        public static double MyFunc2(double x, double a)
+        {
+
+            return a * Math.Sin(x);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            // Создаем новый делегат и передаем ссылку на него в метод Table
+            
+            // Параметры метода и тип возвращаемого значения, должны совпадать с делегатом
+            Fun fun = MyFunc1;
+
+            Console.WriteLine("Таблица функции MyFunc:");
+            Table(fun, -2, 2, 5);
+
+            fun = MyFunc2;
+            Console.WriteLine("Таблица другой функции MyFunc:");
+            Table(fun, -2, 2, 5);
+
+            Console.ReadKey();
+        }
+    }
+}
